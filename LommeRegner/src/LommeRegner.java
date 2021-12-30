654219d4d83e1bd9d6c8b6f440b748c8a4745158
gusse <gustavgjohnsen@gmail.com>
2021-12-30 20:23:11 +0100

    initial commit

---
+++ LommeRegner/src/LommeRegner.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class LommeRegner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int action = -1;
+
+        while (action == -1) {
+            System.out.println("Vil du plus(1), minus(2), gange(3), dividere(4), eller afslutte programmet (0)");
+             action = scanner.nextInt();
+            if (action == 1) {
+                System.out.println("Indtast tal");
+                double x = scanner.nextInt();
+                System.out.println("Indtast andet tal");
+                double y = scanner.nextInt();
+                System.out.println(x + y);
+            } else if (action == 2) {
+                System.out.println("Indtast tal");
+                double x = scanner.nextInt();
+                System.out.println("Indtast andet tal");
+                double y = scanner.nextInt();
+                System.out.println(x - y);
+            } else if (action == 3) {
+                System.out.println("Indtast tal");
+                double x = scanner.nextInt();
+                System.out.println("Indtast andet tal");
+                double y = scanner.nextInt();
+                System.out.println(x * y);
+            } else if (action == 4) {
+                System.out.println("Indtast tal");
+                double x = scanner.nextInt();
+                System.out.println("Indtast andet tal");
+                double y = scanner.nextInt();
+                System.out.println(x / y);
+            } else if (action == 0) {
+                break;
+            } else {
+                action = -1;
+            }
+
+        }
+    }
+}
