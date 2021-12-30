4eaff334c76716539c5eacb3821526c79bd818ff
gusse <gustavgjohnsen@gmail.com>
2021-12-30 21:01:55 +0100

    Gjort så man ikke kan dividere med 0, og at brugeren får en fejl besked
    Givet en luknings besked når brugeren indtaster "0" for at afslutte programmet

--- LommeRegner/src/LommeRegner.java
+++ LommeRegner/src/LommeRegner.java
@@ -7,7 +7,7 @@ public class LommeRegner {
 
         while (action == -1) {
             System.out.println("Vil du plus(1), minus(2), gange(3), dividere(4), opløft i anden(5) eller afslutte programmet (0)");
-             action = scanner.nextInt();
+            action = scanner.nextInt();
             if (action == 1) {
                 System.out.println("Indtast tal");
                 double x = scanner.nextInt();
@@ -33,10 +33,21 @@ public class LommeRegner {
             } else if (action == 4) {
                 System.out.println("Indtast tal");
                 double x = scanner.nextInt();
+                if (x == 0) {
+                    System.out.println("Hov! Du kan ikke dividere med 0.");
+                    System.out.println("Lommeregneren lukker nu, da du har indtastet et ikke gyldigt svar");
+                    break;
+                }
                 System.out.println("Indtast andet tal");
                 double y = scanner.nextInt();
+                if (y == 0) {
+                    System.out.println("Hov! Du kan ikke dividere med 0.");
+                    System.out.println("Lommeregneren lukker nu, da du har indtastet et ikke gyldigt svar");
+                    break;
+                }
                 System.out.println(x / y);
             } else if (action == 0) {
+                System.out.println("Programmet lukker nu");
                 break;
             } else {
                 action = -1;
